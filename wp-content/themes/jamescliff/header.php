61fccdf92289632b6b6db66330e1ef34e0dcd527
James Cliff <me@jamescliff.com>
2020-11-02 12:59:03 -0500

    :bug: fixed title

--- wp-content/themes/jamescliff/header.php
+++ wp-content/themes/jamescliff/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <title><?php wp_title(' '); ?></title>
+    <title><?php wp_title(''); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
